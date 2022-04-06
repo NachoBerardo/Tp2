f8ef4b7dc48e3700c0477cb83acb4ab45f370e21
NachoBerardo <101335543+NachoBerardo@users.noreply.github.com>
2022-04-06 11:49:49 -0300

    Validación de contraseña terminada

---
+++ Assets/ValidarContraseña.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ValidarContraseña : MonoBehaviour
+{
+
+    string ContraseñaCorrecta;
+    string ContraseñaUsuario;
+    public Text IngresoUsuario;
+
+    public void Validar_Contraseña()
+    {
+        
+        ContraseñaUsuario = IngresoUsuario.text;
+
+        if (ContraseñaUsuario == ContraseñaCorrecta)
+        {
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+
+    void Start()
+    {
+        ContraseñaCorrecta = "12345";
+    }
+
+
+}
