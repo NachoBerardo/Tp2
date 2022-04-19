e09d200de1dbcccc1571fb243555741116d7683e
NachoBerardo <101335543+NachoBerardo@users.noreply.github.com>
2022-04-19 11:36:07 -0300

    tp2 terminado
    Pop us puestos y diseño cambiado

--- Assets/ValidarContraseña.cs
+++ Assets/ValidarContraseña.cs
@@ -10,6 +10,8 @@ public class ValidarContraseña : MonoBehaviour
     string ContraseñaCorrecta;
     string ContraseñaUsuario;
     public Text IngresoUsuario;
+    public Text TextoMensaje;
+    public GameObject cartel_Mensaje;
 
     public void Validar_Contraseña()
     {
@@ -19,16 +21,21 @@ public class ValidarContraseña : MonoBehaviour
         if (ContraseñaUsuario == ContraseñaCorrecta)
         {
             Debug.Log("Bienvenido");
+            cartel_Mensaje.SetActive(true);
+            TextoMensaje.text = "Bienvenido";
         }
         else
         {
             Debug.Log("Contraseña incorrecta");
+            cartel_Mensaje.SetActive(true);
+            TextoMensaje.text = "Contraseña incorrecta";
         }
     }
 
     void Start()
     {
         ContraseñaCorrecta = "12345";
+        cartel_Mensaje.SetActive(false);
     }
 
 
